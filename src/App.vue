eca1ead0d9919b7a3b24bc11b04e049a7328f953
rey <reynold.seq@gmail.com>
2022-05-07 14:43:35 +0530

    todo app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,99 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <img alt="Vue logo" src="./assets/logo.png" class="vue-logo">
+    <h1>To Do App</h1>
+    <div class="content">
+      <input type="text" v-model="inputState">
+      <button @click="AddtoList">Add</button>
+    </div>
+  </div>
+  <Todo-list v-for="(list, index) in todoList" :key="index" :todo="list" @delete-item="deleteitem(index)"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TodoList from './components/TodoList.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TodoList
+  },
+  data() {
+    return {
+      inputState : '',
+      todoList: []
+    }
+  },
+  methods: {
+    AddtoList() {
+      if(this.inputState) {
+        this.todoList.push(this.inputState)
+        this.inputState = ''
+      } else {
+        alert('Input Cannot be Empty!!!')
+      }
+    },
+    deleteitem(n) {
+      this.todoList.shift(n, 1)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Macondo&display=swap');
+
+    *, *::before, *::after {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background: #D9E4F5;
+        font-family: 'Macondo', cursive;
+        font-size: 16px;
+    }
+
+    .container {
+      text-align: center;
+    }
+    .vue-logo {
+      height: 12.5rem;
+      width: 12.5rem;
+      margin-top: 1rem;
+    }
+    .content {
+      margin-top: 1.5rem;
+      height: 5rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .content > input {
+      width: 12.5rem;
+      height: 1.875rem;
+      margin-right: 5px;
+    }
+    .content > button {
+      width: 3.75rem;
+      height: 2rem;
+      font-family: 'Macondo', cursive;
+      font-weight: bold;
+      color: white;
+      background: green;
+      border: none;
+      
+    }
+
+    .container > h1 {
+      color: green;
+    }
+
+    @media only screen and (min-width: 600px) {
+        .content > input {
+          width: 18.75rem;
+     
+    }
+  }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="list">
+      <p>{{todo}} <button @click="$emit('delete-item')">Delete</button></p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['todo']
+}
+</script>
+
+<style>
+    .list {
+        text-align: left;
+        text-transform: capitalize;
+    }
+    
+    .list > p {
+        width: 16.875rem;
+        height: 2.5rem;
+        background: green;
+        margin: 20px auto;
+        display: flex;
+        justify-content: space-between;
+        padding: 0rem 2rem;
+        align-items: center;
+        color: white;
+    }
+
+    .list > p > button {
+        width: 3.125rem;
+        height: 1.5625rem;
+        border: none;
+        background: #35495E;
+        color: white;
+        cursor: pointer;
+    }
+
+    @media only screen and (min-width: 37.5rem) {
+        .list > p {
+            width: 29.375rem;
+        }
+    }
+
+</style>
